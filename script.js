b9021e4622bc1779b864cddc9db9d81ffaab5380
IceLily636 <24196ks@hvhs.school.nz>
2026-06-11 21:11:18 +0000

    Jun 11, 2026, 9:11 PM

--- script.js
+++
@@ -1,7 +0,0 @@
-const USER = {}
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-
-function getFormInput(){
-let NAME_FIELD = document.getElementById("nameField");
-USER.name = NAME_FIELD.value;
-OUTPUT.innerHTML += "<p>Your name is"+ USER.name+"</p>";}
