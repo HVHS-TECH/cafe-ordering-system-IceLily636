70c5450c86f9df5b924bb62864c795c02f6f9c97
IceLily636 <24196ks@hvhs.school.nz>
2026-06-11 21:44:04 +0000

    Jun 11, 2026, 9:44 PM

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 console.log("Hello, World!");
 const menuItems = [
+    { name: "Espresso", price: 3.00 },
+    { name: "Cappuccino", price: 4.00 },
+    { name: "Latte", price: 4.50 },
+    { name: "Mocha", price: 5.00 }
+];
+
+function displayMenu() {
