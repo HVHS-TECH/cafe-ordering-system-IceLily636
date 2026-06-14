114bbf048ab3572cd84c06bcc73539fc60268d2a
IceLily636 <24196ks@hvhs.school.nz>
2026-06-14 22:46:57 +0000

    Jun 14, 2026, 10:46 PM

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Hello, World!");
 const OUTPUT = document.getElementById("JavaScriptOutput");
 const menuItems = [
-    { name: "Sakura Mochi", price: 5.00 },
+    { name: "Sakura Mochi", price: 5.00  image: "cafe item 1.pn" },
     { name: "Chocolate", price: 20.00 },
     { name: "Universal Peace", price: 7.00 },
     { name: "Rainbow Macaroons", price: 15.00 }
