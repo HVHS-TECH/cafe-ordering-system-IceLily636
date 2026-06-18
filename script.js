a51d70796f412740062a0e23ab63e34634b4621a
IceLily636 <24196ks@hvhs.school.nz>
2026-06-18 21:27:57 +0000

    Jun 18, 2026, 9:27 PM

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Hello, World!");
 let change
 let order
-function calculateChange(USER.money, _price)
+function calculateChange( _price){}
 return change
 const USER ={};
 const OUTPUT = document.getElementById("JavaScriptOutput");
