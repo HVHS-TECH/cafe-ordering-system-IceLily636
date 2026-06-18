9154ef7aeced9eea523c1b8debbd29cc9d65c195
IceLily636 <24196ks@hvhs.school.nz>
2026-06-18 21:23:48 +0000

    Jun 18, 2026, 9:23 PM

--- script.js
+++ script.js
@@ -34,14 +34,7 @@ const ORDER_FIELD = document.getElementById("orderField")
 order = Number (ORDER_FIELD.value)
 OUTPUT.innerHTML+="<p>You have ordered "+menuItems[order].name+"</p>";}
 //receipt 
-if(USER.money >= menuItems[order].price){
-    OUTPUT.innerHTML +="<p>A chocolate bar is 4 dollars, you can afford it</p>"
-    change = calculateChange(USER.money - menuItems[order].price)
-    OUTPUT.innerHTML +="<p>Your change is" +change+ "Dollars</p>"
-}
-else{
-    OUTPUT.innerHTML +="<p>A" +menuItems[order].name+ "is" +menuItems[order].price+ ",you cant afford it.</p>"
-}
+
 
 
 // show item image
